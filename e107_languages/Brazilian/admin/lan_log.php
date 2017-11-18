7a541c698816317abfa937255e8db8ce5008dfcd
Barbara Samel Rocha Tostes <barbaratostes@gmail.com>
2017-11-18 17:27:22 -0200

    Add files via upload

---
+++ e107_languages/Brazilian/admin/lan_log.php
@@ -0,0 +1,27 @@
+<?php
+/*
+	**** Tradução Original
+|Brazilian_portuguese Translation, 2004
+|Author: Claytom Valle
+|E-mail: claytomv@yahoo.com
+	**** Revisão
+|Revisão da Tradução para Português do Brasil 10/2004
+|Autor: PHPautH e Colaboradores
+|WebSite: http://www.e107.com.br
+*/
+define("LOGLAN_1", "Detalhes de Log atualizados.");
+define("LOGLAN_2", "Tabelas de estatísticas esvaziadas.");
+define("LOGLAN_3", "Ativar contadores e logs?");
+define("LOGLAN_4", "Tipo de log");
+define("LOGLAN_5", "Só Domínio");
+define("LOGLAN_6", "URL completa");
+define("LOGLAN_7", "Contar quantas visitas foram feitas?");
+define("LOGLAN_8", "Limpar as tabelas de estatísticas");
+define("LOGLAN_9", "Clique para confirmar que quer limpar as tabelas");
+define("LOGLAN_10", "Clique para limpar os contadores");
+define("LOGLAN_11", "Limpar!");
+define("LOGLAN_12", "Atualizar os detalhes de Log");
+define("LOGLAN_13", "Definições de Log/Contadores");
+
+
+?>
---
+++ e107_languages/Brazilian/admin/lan_theme_prev.php
@@ -0,0 +1,29 @@
+<?php
+/*
+	**** Tradução Original
+|Brazilian_portuguese Translation, 2004
+|Author: Claytom Valle
+|E-mail: claytomv@yahoo.com
+	**** Revisão
+|Revisão da Tradução para Português do Brasil 10/2004
+|Autor: PHPautH e Colaboradores
+|WebSite: http://www.e107.com.br
+*/
+define("TPVLAN_1", "Esta é uma página de teste de Tema");
+define("TPVLAN_2", "Este tema é ativado apenas para esta página. Os Links abaixo não vão funcionar.");
+define("TPVLAN_3", "Preferências");
+define("TPVLAN_4", "Visualizar mais Temas");
+define("TPVLAN_5", "Visualizar os Temas do site");
+define("TPVLAN_6", "Visualizar");
+define("TPVLAN_7", "Visualizar o Tema");
+define("TPVLAN_8", "Este tema é ativado somente para esta página. Os Links abaixo não funcionarão.");
+define("TPVLAN_9", "Visualizar mais Temas");
+define("TPVLAN_10", "Clique no botão Submit para definir como Tema do site. ");
+define("TPVLAN_11", "informação");
+define("TPVLAN_12", "Preferências do Tema gravadas.");
+define("TPVLAN_13", "Tema");
+define("TPVLAN_15", "Salvar");
+define("TPVLAN_14", "Se um tema não aparecer corretamente, certifique-se de que ele foi enviado com as permissões certas para o servidor. Permissão de leitura.");
+
+
+?>
